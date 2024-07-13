d3f0c413d84fe9a1cc20dd73cb4edb373548b274
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-07-13 20:55:39 +0400

    Add Item model with it's configuration

---
+++ ACA.DeliverySystem.Data/Configurations/ItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ACA.DeliverySystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ACA.DeliverySystem.Data.Configurations
+{
+    public class ItemConfiguration : IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Price).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(300);
+        }
+    }
+}
--- ACA.DeliverySystem.UI/Program.cs
+++ ACA.DeliverySystem.UI/Program.cs
@@ -1,4 +1,6 @@
 using ACA.DeliverySystem.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
