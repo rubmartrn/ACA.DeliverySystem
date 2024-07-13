e6acccbf527e9e9af500b75639a62b686fa3b498
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-07-13 21:21:28 +0400

    Add Item Model

--- ACA.DeliverySystem.Data/Models/Item.cs
+++ ACA.DeliverySystem.Data/Models/Item.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ACA.DeliverySystem.Data
+﻿namespace ACA.DeliverySystem.Data.Models
 {
-    internal class Item
+    public class Item
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
