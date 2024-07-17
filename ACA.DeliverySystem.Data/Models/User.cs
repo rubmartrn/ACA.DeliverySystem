ac7b0622774e1506817c2a7cfab1e09475007633
edyegh <edgar_egiazaryan_04@mail.ru>
2024-07-17 19:58:46 +0400

    Add User

---
+++ ACA.DeliverySystem.Data/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACA.DeliverySystem.Data.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+        public string SureName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
