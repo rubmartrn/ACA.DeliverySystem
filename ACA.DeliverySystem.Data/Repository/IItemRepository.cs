679bb7d4e1cea520c9e6dfe193a594f6a0da1342
Aram <aram050385@gmail.com>
2024-07-29 21:34:26 +0400

    Add Item Repository

---
+++ ACA.DeliverySystem.Data/Repository/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using ACA.DeliverySystem.Data.Models;
+
+namespace ACA.DeliverySystem.Data.Repository
+{
+    public interface IItemRepository
+    {
+        Task Add(Item item, CancellationToken token);
+        Task Delete(int id, CancellationToken token);
+        Task<IEnumerable<Item>> GetAllItem(CancellationToken token);
+        Task<Item> GetItemById(int id, CancellationToken token);
+        Task Update(Item item, CancellationToken token);
+    }
+}
---
+++ ACA.DeliverySystem.Data/Repository/ItemRepository.cs
@@ -0,0 +1,51 @@
+﻿using ACA.DeliverySystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACA.DeliverySystem.Data.Repository
+{
+    public class ItemRepository : IItemRepository
+    {
+        readonly DeliveryDbContext _context;
+        public ItemRepository(DeliveryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Item>> GetAllItem(CancellationToken token)
+        {
+            return await _context.Items.ToListAsync(token);
+        }
+
+        public async Task<Item> GetItemById(int id, CancellationToken token)
+        {
+            return await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Add(Item item, CancellationToken token)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Item item, CancellationToken token)
+        {
+            _context.Items.Update(item);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task Delete(int id, CancellationToken token)
+        {
+            var student = await _context.Items.SingleAsync(e => e.Id == id, token);
+            _context.Items.Remove(student);
+            await _context.SaveChangesAsync(token);
+
+        }
+
+    }
+}
