5c3d92429d3b2aa90284e5e3d5746498325d245c
Aram <aram050385@gmail.com>
2024-08-01 17:13:31 +0400

    Add UnitOfWorkTests

---
+++ ACA.DeliverySystem.Data.Test/UnitOfWorkTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACA.DeliverySystem.Data.Test
+{
+    public class UnitOfWorkTests
+    {
+    }
+}
