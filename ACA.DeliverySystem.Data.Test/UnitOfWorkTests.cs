70f54d1696130d94e9ce032c0a8e5b14faedda2c
Aram <aram050385@gmail.com>
2024-08-01 17:50:12 +0400

    Add  NuGet Moq

--- ACA.DeliverySystem.Data.Test/UnitOfWorkTests.cs
+++ ACA.DeliverySystem.Data.Test/UnitOfWorkTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using ACA.DeliverySystem.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace ACA.DeliverySystem.Data.Test
 {
     public class UnitOfWorkTests
     {
+       
     }
 }
