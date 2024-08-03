2752fbfa3927c91160c06a7a80654921f6f7fc1f
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-08-03 11:10:37 +0400

    Update

--- ACA.DeliverySystem.Business/Services/IOrderService.cs
+++ ACA.DeliverySystem.Business/Services/IOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace ACA.DeliverySystem.Business.Services
+﻿using ACA.DeliverySystem.Data.Models;
+
+namespace ACA.DeliverySystem.Business.Services
 {
     public interface IOrderService
     {
+        Task<Order> CreateOrder(Order order, CancellationToken token);
+        Task Delete(int id, CancellationToken token);
+        Task<Order> Get(int id, CancellationToken token);
+        Task<IEnumerable<Order>> GetAll(CancellationToken token);
+        Task<bool> Update(int id, Order model, CancellationToken token);
     }
 }
