a72049f9c553f5478a0691aa03c1e2bf45a9b470
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-08-05 01:31:11 +0400

    Updating migrations

---
+++ ACA.DeliverySystem.Data/Migrations/20240804211640_Update.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ACA.DeliverySystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OrderId",
+                table: "Users",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Orders_OrderId",
+                table: "Users",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Orders_OrderId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_OrderId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Orders_OrderId",
+                table: "Items",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+    }
+}
