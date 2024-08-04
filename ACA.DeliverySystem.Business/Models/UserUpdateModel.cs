2af9bfe089e321c1f600a8b2eca9eb4e5422450b
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-08-05 01:30:27 +0400

    Update userModel

--- ACA.DeliverySystem.Business/Models/UserUpdateModel.cs
+++ ACA.DeliverySystem.Business/Models/UserUpdateModel.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ACA.DeliverySystem.Business.Models
+﻿namespace ACA.DeliverySystem.Business.Models
 {
     public class UserUpdateModel
     {
-
-        public int OrderId { get; set; }
 
         public string Name { get; set; }
 
         public string SureName { get; set; }
 
-        [EmailAddress]
-        public string Email { get; set; }
     }
 }
