27490fc5e61995d91555b4d13316afadb9164c95
edyegh <edgar_egiazaryan_04@mail.ru>
2024-08-04 20:30:27 +0400

    Fix Bugs

--- ACA.DeliverySystem.Data/Repository/IOrderRepository.cs
+++ ACA.DeliverySystem.Data/Repository/IOrderRepository.cs
@@ -4,7 +4,7 @@ namespace ACA.DeliverySystem.Data.Repository
 {
     public interface IOrderRepository
     {
-        Task Add(Order order, CancellationToken token);
+        void Add(Order order);
         Task Delete(int id, CancellationToken token);
         Task<IEnumerable<Order>> GetAll(CancellationToken token);
         Task<Order> Get(int id, CancellationToken token);
