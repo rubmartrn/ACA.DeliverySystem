bd0163d2a5318d6ff3692f16f1b2ea506052fba4
edyegh <edgar_egiazaryan_04@mail.ru>
2024-08-09 20:57:46 +0400

    Add Item Service Frontend

---
+++ ACA.DeliverySystem.UI/Services/ItemService.cs
@@ -0,0 +1,25 @@
+﻿using ACA.DeliverySystem.UI.Pages;
+using System.Net.Http.Json;
+
+namespace ACA.DeliverySystem.UI.Services
+{
+    public class ItemService
+    {
+        private readonly HttpClient _client;
+
+        public ItemService(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task<IEnumerable<Item>> TestGetAllItems()
+        {
+            return await _client.GetFromJsonAsync<List<Item>>("Item");
+        }
+
+
+     
+
+    }
+}
