e24451a461bd6e80930ee430babad8eebfff583b
edyegh <edgar_egiazaryan_04@mail.ru>
2024-08-11 20:33:07 +0400

    ItemService for Frontend

---
+++ ACA.DeliverySystem.UI/Models/ItemUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ACA.DeliverySystem.Business.Models
+{
+    public class ItemUpdateModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- ACA.DeliverySystem.UI/Services/ItemService.cs
+++ ACA.DeliverySystem.UI/Services/ItemService.cs
@@ -1,4 +1,6 @@
-﻿using ACA.DeliverySystem.UI.Pages;
+﻿using ACA.DeliverySystem.Business.Models;
+using ACA.DeliverySystem.UI.Models;
+using ACA.DeliverySystem.UI.Pages;
 using System.Net.Http.Json;
 
 namespace ACA.DeliverySystem.UI.Services
@@ -13,13 +15,51 @@ namespace ACA.DeliverySystem.UI.Services
         }
 
 
-        public async Task<IEnumerable<Item>> TestGetAllItems()
+        //Read
+        public async Task<IEnumerable<Item>> GetAll()
         {
             return await _client.GetFromJsonAsync<List<Item>>("Item");
         }
 
-
+        public async Task<Item> GetbyId(int id)
+        {
+            return await _client.GetFromJsonAsync<Item>($"Item/{id}");
+        }
      
+        //Create
+        public async Task<OperationResult> Add(ItemAddModel model)
+        {
+            var response = await _client.PostAsJsonAsync("Item", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+
+            return OperationResult.Fail(response.ReasonPhrase!);
+        }
+
+        //Delete
+
+        public async Task Delete(int id)
+        {
+            await _client.DeleteAsync($"Item/{id}");
+        }
+
+        //Update
+
+        public async  Task<OperationResult> Update(int id, ItemUpdateModel model)
+        {
+            var response = await _client.PutAsJsonAsync($"Item/{id}", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+
+            return OperationResult.Fail(response.ReasonPhrase!);
+
+        }
 
     }
 }
