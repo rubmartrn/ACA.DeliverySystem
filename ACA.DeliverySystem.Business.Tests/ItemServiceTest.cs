c8c4fbd509613c554338b509dd25f01b99edfcd7
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-08-18 14:36:00 +0400

    Uncomment

--- ACA.DeliverySystem.Business.Tests/ItemServiceTest.cs
+++ ACA.DeliverySystem.Business.Tests/ItemServiceTest.cs
@@ -1,101 +1,101 @@
-﻿//using ACA.DeliverySystem.Business.Services;
-//using ACA.DeliverySystem.Data;
-//using ACA.DeliverySystem.Data.Models;
-//using ACA.DeliverySystem.Data.Repository;
-//using Moq;
+﻿using ACA.DeliverySystem.Business.Services;
+using ACA.DeliverySystem.Data;
+using ACA.DeliverySystem.Data.Models;
+using ACA.DeliverySystem.Data.Repository;
+using Moq;
 
-//namespace ACA.DeliverySystem.Business.Tests
-//{
-//    public class ItemServiceTest
-//    {
-//        private Mock<Order> _mockOrder = new Mock<Order>();
-//        private Mock<IItemRepository> _iItemRepositoryMock = new Mock<IItemRepository>();
-//        private Mock<IUnitOfWork> _uowMock = new Mock<IUnitOfWork>();
-//        private IItemService service => new ItemService(_uowMock.Object);
+namespace ACA.DeliverySystem.Business.Tests
+{
+    public class ItemServiceTest
+    {
+        private Mock<Order> _mockOrder = new Mock<Order>();
+        private Mock<IItemRepository> _iItemRepositoryMock = new Mock<IItemRepository>();
+        private Mock<IUnitOfWork> _uowMock = new Mock<IUnitOfWork>();
+        private IItemService service => new ItemService(_uowMock.Object);
 
-//        [Fact]
+        [Fact]
 
-//        public async Task CreateItem_Success()
-//        {
-//            //Arrange
-//            var items = new List<Item>();
-//            var item = new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object };
-//            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
-//            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
-//            //Act
-//            await service.CreateItem(item, CancellationToken.None);
-//            //Assert
-//            _iItemRepositoryMock.Verify(m => m.Add(It.Is<Item>(c => c == item), It.IsAny<CancellationToken>()), Times.Once);
-//        }
+        public async Task CreateItem_Success()
+        {
+            //Arrange
+            var items = new List<Item>();
+            var item = new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object };
+            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
+            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
+            //Act
+            await service.CreateItem(item, CancellationToken.None);
+            //Assert
+            _iItemRepositoryMock.Verify(m => m.Add(It.Is<Item>(c => c == item), It.IsAny<CancellationToken>()), Times.Once);
+        }
 
-//        [Fact]
-//        public async Task Delete_Success()
-//        {
-//            //Arrange
-//            var items = new List<Item>
-//            {
-//                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
-//                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
-//            };
-//            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
-//            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
-//            //Act
-//            await service.Delete(1, CancellationToken.None);
-//            _iItemRepositoryMock.Verify(m => m.Delete(It.Is<int>(c => c == 1), It.IsAny<CancellationToken>()), Times.Once());
-//        }
+        [Fact]
+        public async Task Delete_Success()
+        {
+            //Arrange
+            var items = new List<Item>
+            {
+                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
+                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
+            };
+            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
+            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
+            //Act
+            await service.Delete(1, CancellationToken.None);
+            _iItemRepositoryMock.Verify(m => m.Delete(It.Is<int>(c => c == 1), It.IsAny<CancellationToken>()), Times.Once());
+        }
 
-//        [Fact]
-//        public async Task GetAllItems_Success()
-//        {
-//            //Arrange
-//            var items = new List<Item>
-//            {
-//                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
-//                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
-//            };
-//            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
-//            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
-//            //Act
-//            var result = await service.GetAll(CancellationToken.None);
-//            //Assert
-//            Assert.Equal(2, result.Count());
+        [Fact]
+        public async Task GetAllItems_Success()
+        {
+            //Arrange
+            var items = new List<Item>
+            {
+                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
+                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
+            };
+            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
+            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
+            //Act
+            var result = await service.GetAll(CancellationToken.None);
+            //Assert
+            Assert.Equal(2, result.Count());
 
-//        }
+        }
 
-//        [Fact]
-//        public async Task GetItemById_Success()
-//        {
-//            //Arrange
-//            var item = new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object };
-//            _iItemRepositoryMock.Setup(e => e.GetById(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(item);
+        [Fact]
+        public async Task GetItemById_Success()
+        {
+            //Arrange
+            var item = new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object };
+            _iItemRepositoryMock.Setup(e => e.GetById(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(item);
 
-//            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
-//            // Act
+            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
+            // Act
 
-//            await service.GetById(1, CancellationToken.None);
+            await service.GetById(1, CancellationToken.None);
 
-//            //Assert
-//            _iItemRepositoryMock.Verify(m => m.GetById(It.Is<int>(c => c == 1), It.IsAny<CancellationToken>()), Times.Once);
-//        }
+            //Assert
+            _iItemRepositoryMock.Verify(m => m.GetById(It.Is<int>(c => c == 1), It.IsAny<CancellationToken>()), Times.Once);
+        }
 
-//        [Fact]
-//        public async Task Update()
-//        {
-//            //Arrange
-//            var items = new List<Item>
-//            {
-//                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
-//                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
-//            };
-//            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
-//            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
-//            var currentItem = await service.GetById(1, CancellationToken.None);
-//            //Act
+        [Fact]
+        public async Task Update()
+        {
+            //Arrange
+            var items = new List<Item>
+            {
+                new Item { Id = 1, OrderId = 1, Name = "fast food", Description = "5 pies", Price = 25, Order = _mockOrder.Object },
+                new Item { Id = 2, OrderId = 1, Name = "Donar", Description = "6 pies", Price = 35, Order = _mockOrder.Object },
+            };
+            _iItemRepositoryMock.Setup(e => e.GetAll(It.IsAny<CancellationToken>())).ReturnsAsync(items);
+            _uowMock.Setup(u => u.ItemRepository).Returns(_iItemRepositoryMock.Object);
+            var currentItem = await service.GetById(1, CancellationToken.None);
+            //Act
 
-//            await service.Update(currentItem, CancellationToken.None);
-//            //Assert
-//            _iItemRepositoryMock.Verify(m => m.Update(It.Is<Item>(c => c == currentItem), It.IsAny<CancellationToken>()), Times.Once);
+            await service.Update(currentItem, CancellationToken.None);
+            //Assert
+            _iItemRepositoryMock.Verify(m => m.Update(It.Is<Item>(c => c == currentItem), It.IsAny<CancellationToken>()), Times.Once);
 
-//        }
-//    }
-//}
+        }
+    }
+}
