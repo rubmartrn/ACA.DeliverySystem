c4e495b7d420622da72b25bc60c37de9d6874b1a
edyegh <edgar_egiazaryan_04@mail.ru>
2024-08-20 00:37:45 +0400

    Update

---
+++ ACA.DeliverySystem.UI/Models/OrderAddModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ACA.DeliverySystem.UI.Models
+{
+    public class OrderAddModel
+    {
+        public string Name { get; set; }
+    }
+}
--- ACA.DeliverySystem.UI/Services/UserService.cs
+++ ACA.DeliverySystem.UI/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using ACA.DeliverySystem.UI.Models;
+using ACA.DeliverySystem.UI.Pages;
 using System.Net.Http.Json;
 
 namespace ACA.DeliverySystem.UI.Services
@@ -12,9 +13,85 @@ namespace ACA.DeliverySystem.UI.Services
             _client = Client;
         }
 
-        public async Task Create(UserAddModel model)
+        public async Task<OperationResult> Create(UserAddModel model)
         {
-            var response = _client.PostAsJsonAsync<User>
+            var response = await _client.PostAsJsonAsync("User", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+            else
+            {
+                return new OperationResult
+                {
+                    Success = false,
+                    ErrorMessage = "Failed to create the user."
+                };
+            }
+        }
+
+        public async Task<OperationResult> Update(int id, UserUpdateModel model)
+        {
+            var response = await _client.PutAsJsonAsync($"User?id={id}", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+            else
+            {
+                return new OperationResult
+                {
+                    Success = false,
+                    ErrorMessage = "Failed to update the user."
+                };
+            }
+        }
+
+
+        public async  Task<OperationResult> Delete(int id)
+        {
+            var response = await _client.DeleteAsync($"User?id={id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+            else
+            {
+                return new OperationResult
+                {
+                    Success = false,
+                    ErrorMessage = "Failed to delete the user."
+                };
+            }
+        }
+
+
+        public async Task<IEnumerable<Order>> GetUserOrders(int userId)
+        {
+            return await _client.GetFromJsonAsync<List<Order>>($"User/{userId}/orders");
+        }
+
+
+
+        public async Task<OperationResult> AddOrderInUser(int userId, OrderAddModel model)
+        {
+            var response = await _client.PostAsJsonAsync($"User/{userId}/orders", model);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return OperationResult.Ok();
+            }
+            else
+            {
+                return new OperationResult
+                {
+                    Success = false,
+                    ErrorMessage = "Failed to delete the user."
+                };
+            }
         }
 
 
