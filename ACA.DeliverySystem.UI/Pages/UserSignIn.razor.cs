d0c7a2f4257818be785bb5d151e2023f5c39607d
vardzaqevosyan <vardzaqevosyan@gmail.com>
2024-09-14 11:42:00 +0400

    sign in password visibility

--- ACA.DeliverySystem.UI/Pages/UserSignIn.razor.cs
+++ ACA.DeliverySystem.UI/Pages/UserSignIn.razor.cs
@@ -9,6 +9,12 @@ namespace ACA.DeliverySystem.UI.Pages
     {
         protected SignInRequestModel? _loginModel { get; set; }
         protected string? _errorMessage;
+
+        // Variables to control password input type and icon
+        protected string _passwordInputType = "password";
+        protected string _passwordIcon = Icons.Material.Filled.VisibilityOff;
+
+
         [Inject] protected UserService UserService { get; set; } = default!;
         [Inject] protected NavigationManager NavigationManager { get; set; } = default!;
         [Inject] protected ISnackbar Snackbar { get; set; } = default!;
@@ -30,6 +36,21 @@ namespace ACA.DeliverySystem.UI.Pages
             else
             {
                 _errorMessage = result.ErrorMessage!;
+            }
+        }
+
+        // Toggle password visibility
+        protected void TogglePasswordVisibility()
+        {
+            if (_passwordInputType == "password")
+            {
+                _passwordInputType = "text";
+                _passwordIcon = Icons.Material.Filled.Visibility;
+            }
+            else
+            {
+                _passwordInputType = "password";
+                _passwordIcon = Icons.Material.Filled.VisibilityOff;
             }
         }
     }
